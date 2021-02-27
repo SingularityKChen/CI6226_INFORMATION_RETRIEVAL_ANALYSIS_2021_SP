1fe61b5326f5f805e8bcba13be7151c0480fcf42
singularity <chency_singularity@163.com>
2021-02-27 20:30:39 +0800

    feat: tokenize one block of data instead of all blocks
    + use start and end document id to read and tokenize them;

--- indexing_get_tokens.py
+++ indexing_get_tokens.py
@@ -7,18 +7,16 @@ from nltk.stem import PorterStemmer
 
 
 class indexing_get_tokens:
-    def __init__(self, f_sort_dir, f_block_size=666):
+    def __init__(self, f_sort_dir):
         self.sort_dir = Path(f_sort_dir)
-        self.block_size = f_block_size
         self.stop_word = set(stopwords.words("english"))
         self.tokenizer = RegexpTokenizer(r'\s+', gaps=True)
         self.debug = True
 
-    def reading_files(self):
-        _blocks_tokens = []
+    def reading_files(self, f_start_id, f_end_id):
         _tokens = []
-        for file_name in [self.sort_dir / "1.txt"] if self.debug else self.sort_dir.iterdir():
-            document_id = int(re.split("\.|/", string=str(file_name)).pop(-2))
+        for doc_id in range(f_start_id + 1, f_end_id + 1):
+            file_name = self.sort_dir / ("%d.txt" % doc_id)
             print("[INFO] current Path %s" % file_name)
             email = file_name.open().read()
             # print("[INFO] Doc is", email)
@@ -34,19 +32,13 @@ class indexing_get_tokens:
             # print("[INFO] After removing stop words:\n", _terms)
             _terms = [term for term in _terms if not term.replace(",", "").replace(".", "").isdigit()]
             # print("[INFO] After removing numbers:\n", _terms)
-            _token_doc_id = [(term, document_id) for term in _terms]
+            _token_doc_id = [(term, doc_id) for term in _terms]
             _tokens.extend(_token_doc_id)
-            if document_id % self.block_size == 0:
-                _blocks_tokens.append(_tokens)
-                _tokens = []
-        if _tokens:
-            _blocks_tokens.append(_tokens)
-        return _blocks_tokens
+        return _tokens
 
 
 if __name__ == '__main__':
     sort_dir = "./docs/HillaryEmails"
     get_tokens = indexing_get_tokens(f_sort_dir=sort_dir)
-    tokens = get_tokens.reading_files()
-    print("[INFO] There are %d tokens from %d blocks" %
-          (sum([len(block_token) for block_token in tokens]), len(tokens)))
+    tokens = get_tokens.reading_files(f_start_id=0, f_end_id=5)
+    print("[INFO] There are %d tokens" % len(tokens))
