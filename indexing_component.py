ae65143c23a913585e7135db7d5d1f4b339e69de
singularity <chency_singularity@163.com>
2021-03-03 12:36:56 +0800

    feat: add @profile to get mem requires

--- indexing_component.py
+++ indexing_component.py
@@ -1,5 +1,6 @@
 from indexing_get_tokens import indexing_get_tokens
 from indexing_SPIMI import indexing_SPIMI
+# from memory_profiler import profile
 from pathlib import Path
 import math
 
@@ -13,25 +14,29 @@ class indexing_component:
         self.get_tokens = indexing_get_tokens(f_sort_dir=f_sort_dir)
         self.spimi = indexing_SPIMI(f_output_dir="")
 
+    @profile
+    # @profile(precision=4, stream=open("./docs/perf/single_block_memory.log", 'w+'))
     def process_one_block(self, f_start_id, f_end_id):
         _tokens = self.get_tokens.reading_files(f_start_id=f_start_id, f_end_id=f_end_id)
         _sored_terms, _dictionary = self.spimi.spimi_invert(f_token_stream=_tokens)
-        print("[INFO] sorted terms:\n", _sored_terms)
-        print("\tdictionary:\n", _dictionary)
-        print("[INFO] There are %d terms" % len(_dictionary))
+        # print("[INFO] sorted terms:\n", _sored_terms)
+        # print("\tdictionary:\n", _dictionary)
+        # print("[INFO] There are %d terms" % len(_dictionary))
         return _dictionary
 
+    @profile
+    # @profile(precision=4, stream=open("./docs/perf/multi_block_memory.log", 'w+'))
     def process_multiple_block(self):
-        print("[INFO] There are %d documents" % self.doc_num)
+        # print("[INFO] There are %d documents" % self.doc_num)
         _loop_num = int(self.doc_num / self.block_size)
         _merge_layers = math.ceil(math.log2(_loop_num))
         _dic_lists = []
         for _block_idx in range(_loop_num):
-            print("[INFO] Block Id %d" % _block_idx)
+            # print("[INFO] Block Id %d" % _block_idx)
             _dic_lists.append(self.process_one_block(f_start_id=_block_idx * self.block_size,
                                                      f_end_id=(_block_idx + 1) * self.block_size - 1))
         if self.doc_num % self.block_size != 0:
-            print("[INFO] processing remaining documents.")
+            # print("[INFO] processing remaining documents.")
             _dic_lists.append(self.process_one_block(f_start_id=_loop_num * self.block_size, f_end_id=self.doc_num - 1))
         while len(_dic_lists) != 1:
             _dic_lists.append(self.spimi.merge_two_blocks(_dic_1=_dic_lists.pop(0), _dic_2=_dic_lists.pop(0)))
@@ -42,5 +47,5 @@ if __name__ == '__main__':
     sort_dir = "./docs/HillaryEmails"
     indexing = indexing_component(f_sort_dir=sort_dir, f_block_size=666)
     dic = indexing.process_multiple_block()
-    print(dic)
-    print(len(dic))
+    # print(dic)
+    # print(len(dic))
