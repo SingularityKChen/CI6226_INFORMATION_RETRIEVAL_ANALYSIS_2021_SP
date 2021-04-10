086d1be81230275d100cd522307569aae74e575d
singularity <chency_singularity@163.com>
2021-04-10 23:43:06 +0800

    feat: compute tf_td using tokenizer;

--- ranking_BM25.py
+++ ranking_BM25.py
@@ -1,25 +1,30 @@
 from math import log10
 from re import split
+from pathlib import Path
+from utils import tokenize
 
 
-class ranking_BM25:
-    def __init__(self, f_k1, f_b, f_n, f_length_filename):
+class ranking_BM25(tokenize):
+    def __init__(self, f_k1, f_b, f_n, f_index_dir, f_length_filename):
         """
 
         :param f_k1:
         :param f_b:
         :param f_n: number of documents in the database
         """
+        super(ranking_BM25, self).__init__()
         self.k = f_k1
         self.b = f_b
         self.n = f_n
-        self.doc_len_file = f_length_filename
+        self.tf_td = {}
         self.l_d = {}
         self.l_avg = 0
+        self.doc_len_file = f_length_filename
+        self.index_dir = f_index_dir
         self.init_doc_length()
         self.init_doc_avg_len()
 
-    def idf(self, f_posting_list):
+    def compute_idf(self, f_posting_list):
         """
         Compute the idf of term in the document d
         :return:
@@ -29,12 +34,16 @@ class ranking_BM25:
         else:
             return 0
 
-    def tf_td(self):
+    def compute_tf_td(self, f_term, f_doc_id):
         """
         Compute term frequency in document d
         :return: term frequency in document d
         """
-        pass
+        _doc = Path(self.index_dir) / ("%d.txt" % f_doc_id)
+        _doc = _doc.open().read()
+        _terms = self.tokenize_text(f_text=_doc)
+        print(_terms)
+        return _terms.count(f_term)
 
     def init_doc_length(self):
         """
@@ -63,10 +72,9 @@ class ranking_BM25:
 
 
 if __name__ == '__main__':
-    from pathlib import Path
     sort_dir = "./docs/HillaryEmails"
     doc_length_filename = "./docs/output/document_length.txt"
     k1 = 0.5
     b = 0.5
     n = len(list(Path(sort_dir).iterdir()))
-    bm25 = ranking_BM25(f_k1=k1, f_b=b, f_n=n, f_length_filename=doc_length_filename)
+    bm25 = ranking_BM25(f_k1=k1, f_b=b, f_n=n, f_index_dir=sort_dir, f_length_filename=doc_length_filename)
